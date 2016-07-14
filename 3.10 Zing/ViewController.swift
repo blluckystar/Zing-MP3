7b7deecaef653420aad553d385d6197352f0f607
A A Ba Loi Malaysia 1 (ip6s+) <trinhbaloi@petronas.com.my>
2016-07-15 03:39:33 +0800

    Initial Commit

---
+++ 3.10 Zing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3.10 Zing
+//
+//  Created by Ba Loi Mac on 7/15/16.
+//  Copyright © 2016 AloCentral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
