1a9f6bf76ee81a6e3231c9c5db99f931c2d5ac6f
A A Ba Loi Malaysia 1 (ip6s+) <trinhbaloi@petronas.com.my>
2016-07-15 04:48:14 +0800

    1st time commit

--- 3.10 Zing/ViewController.swift
+++ 3.10 Zing/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var text: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
+        logo!.alpha = 0
+        text!.alpha = 0
+        
+        
+        
     }
+    //TAT CA DEU BEN TRONG CLASS VIEWCONTROLLER, xoa cai defaul override, them 1 cai override moi
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        UIView.animateWithDuration(4, animations: {
+            self.logo!.alpha = 1
+            })
+        { (finish) in
+            
+            UIView.animateWithDuration(2, animations: {
+                
+                self.text!.center = CGPointMake(self.logo!.center.x, 70)
+                self.text!.alpha = 1
+            })
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        }
     }
-
 
 }
-
